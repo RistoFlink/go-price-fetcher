4a74d93a65bb98bf94b828f4e4850001de0ee934
RistoFlink <kenssi@gmail.com>
2023-10-02 16:12:44 +0300

    Improve error handling

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"log"
+	"main/client"
 )
 
 func main() {
+	client := client.New("http://localhost:3000")
+	price, err := client.FetchPrice(context.Background(), "ET")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v\n", price)
+	return
+
 	listenAddr := flag.String("listenaddr", ":3000", "The service is running on address")
 	flag.Parse()
 
